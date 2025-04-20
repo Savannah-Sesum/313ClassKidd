7490a8fbb2eb3facb28b013aed82848cf5bcb2cc
Savannah S <savannahsesum@gmail.com>
2025-04-19 22:39:00 -0400

    Create matrix.hpp
    the header ++ file specifically for the testMatrix.cpp

---
+++ HW 8/matrix.hpp
@@ -0,0 +1,34 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Matrix {
+public:
+    // Constructor
+    Matrix(int rows, int cols);
+    // Transposer
+    Matrix transpose() const;
+    // 4 below are overloaded operators
+    // Matrix Addition
+    Matrix operator+(const Matrix &otherMatrix) const;
+    // Matrix on Matrix Multiplication
+    Matrix operator*(const Matrix &otherMatrix) const;
+    // Friend - Matrix Scalar Multiplication (when scalar on left)
+    friend Matrix operator*(int scalar, const Matrix &otherMatrix);
+    // Matrix Scalar Multiplication (when scalar on right)
+    Matrix operator*(int scalar);
+    // Printer
+    void print() const;
+    // For testing purposes
+    // Getter
+    int getData(int rows, int cols) const;
+    // Setter
+    void setData(int rows, int cols, int value);
+private:
+    vector<vector<int>> m_data; // The Matrix
+    int m_rows;
+    int m_cols;
+};
+#endif
